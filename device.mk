a4c098ace9a69719aa268a7c31784ece08bad612
K9100ii <k9100ii.ki@gmail.com>
2022-10-26 00:25:58 +0100

    gtaxlwifi: Switch system property in *.mk to vendor property in *.prop
    Change-Id: Ib312b5de284c7946cf67eeb67183f8e110db356d

--- device.mk
+++ device.mk
@@ -32,9 +32,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Properties
--include $(LOCAL_PATH)/system_prop.mk
-
 # Inherit from gtaxl-common
 $(call inherit-product, device/samsung/gtaxl-common/common.mk)
 
