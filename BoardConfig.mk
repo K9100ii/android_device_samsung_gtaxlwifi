d37f31f4b3e4e99d81217b9d987beaaf97285d66
K9100ii <k9100ii.ki@gmail.com>
2024-02-07 10:39:11 +0000

    gtaxlwifi: Get rid of explicit LOCAL_PATH definition in device.mk
    And also, switch the LOCAL_PATH definition in BoardConfig.mk to
    DEVICE_PATH.
    
    Change-Id: I9f5e27475369f884bbd2918a7e949295b8c6610e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@
 # Inherit from gtaxl-common
 include device/samsung/gtaxl-common/BoardConfigCommon.mk
 
-LOCAL_PATH := device/samsung/gtaxlwifi
+DEVICE_PATH := device/samsung/gtaxlwifi
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineage-gtaxlwifi_defconfig
@@ -30,4 +30,4 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3145728000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 10737418240
 
 # Properties
-TARGET_VENDOR_PROP += $(LOCAL_PATH)/vendor.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
